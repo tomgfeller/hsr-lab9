8fff77b5e55cfc7f744608336d5449f9f740bb80
Tom <thomas.gfeller@axians.ch>
2019-03-26 13:22:40 +0100

    Aufgabe 1.7 Multi Device generate_config.py

--- generate_config.py
+++ generate_config.py
@@ -1,4 +1,3 @@
-
 from jinja2 import Environment, FileSystemLoader
 import yaml
 import pprint
@@ -6,44 +5,45 @@ from datetime import date
 from datetime import datetime
 import napalm
 from time import sleep
+import git
+from git import repo
 
-#Read YAML File into Python Dict
+# Read YAML File into Python Dict
 yaml_file = open('config-device1')
 config = yaml.load(yaml_file)
 
-#pprint.pprint(config)
+# pprint.pprint(config)
 
-#Load Jinja2 template
-env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+# Load Jinja2 template
+env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 template = env.get_template('template.j2')
 
+# The While Loop runs all the code every 60 Seconds --> Compliance Check
 while True:
-    #Render template using the config ,print and save the output
+
+    # Render template using the config ,print and save the output
     for device in config:
-        print("===============================")
+        print("==============================================================")
         rendered_config = template.render(device)
-    #Save Output to File with todays date and Hostname as filename
+        # Save Output to File with todays date and Hostname as filename
         saveoutput = open(str(date.today()) + '-' + device['hostname'] + '.txt', 'w')
         saveoutput.write(rendered_config)
         saveoutput.write('\n')
         saveoutput.close()
         print(rendered_config)
-        print("===============================")
+        print("==============================================================")
 
+        #Pull connection_address attribute from the yaml config-device1 file
+        connection_address = device['connection_address']
 
-    #NAPALM Driver Cred to Access Cisco IOS Device
-    devicelist = ['10.3.255.102']
-
-
-    #Loop trough each IP Address and validate the current config against the template
-    for ip_address in devicelist:
-        print (str(datetime.now()) + " Connecting to " + str(ip_address))
+        #Connection to the devices
+        print(str(datetime.now()) + " Connecting to " + str(connection_address))
         print("==============================================================")
         driver = napalm.get_network_driver('ios')
-        ios = driver(ip_address, 'python', 'cisco')
+        ios = driver(connection_address, 'python', 'cisco')
         ios.open()
-        #Replacing the config iff the difference is > than 0 characters
-        ios.load_replace_candidate(config= rendered_config)
+        # Replacing the config if the difference is > than 0 characters
+        ios.load_replace_candidate(config=rendered_config)
         diffs = ios.compare_config()
         if len(diffs) > 0:
             print('config has changed')
