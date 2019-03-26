4b0b7f01c6205927ddc7b64bc1757d5094d33993
Tom <thomas.gfeller@axians.ch>
2019-03-26 11:09:00 +0100

    Changed genertate_config Script to Loop every 60 Seconds

--- generate_config.py
+++ generate_config.py
@@ -3,7 +3,9 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import pprint
 from datetime import date
+from datetime import datetime
 import napalm
+from time import sleep
 
 #Read YAML File into Python Dict
 yaml_file = open('config-device1')
@@ -15,39 +17,42 @@ config = yaml.load(yaml_file)
 env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
 template = env.get_template('template.j2')
 
-#Render template using the config ,print and save the output
-for device in config:
-    print("===============================")
-    rendered_config = template.render(device)
-#Save Output to File with todays date and Hostname as filename
-    saveoutput = open(str(date.today()) + '-' + device['hostname'] + '.txt', 'w')
-    saveoutput.write(rendered_config)
-    saveoutput.write('\n')
-    saveoutput.close()
-    print(rendered_config)
-    print("===============================")
-
-
-#NAPALM Driver Cred to Access Cisco IOS Device
-devicelist = ['10.3.255.102']
-
-
-#Loop trough each IP Address and validate the current config against the template
-for ip_address in devicelist:
-    print ("Connecting to " + str(ip_address))
-    print("===============================")
-    driver = napalm.get_network_driver('ios')
-    ios = driver(ip_address, 'python', 'cisco')
-    ios.open()
-    ios.load_replace_candidate(config= rendered_config)
-    diffs = ios.compare_config()
-    if len(diffs) > 0:
-        print('config has changed')
+while True:
+    #Render template using the config ,print and save the output
+    for device in config:
         print("===============================")
-        print("===========Changes:============")
-        print(diffs)
+        rendered_config = template.render(device)
+    #Save Output to File with todays date and Hostname as filename
+        saveoutput = open(str(date.today()) + '-' + device['hostname'] + '.txt', 'w')
+        saveoutput.write(rendered_config)
+        saveoutput.write('\n')
+        saveoutput.close()
+        print(rendered_config)
         print("===============================")
-        ios.commit_config()
-    else:
-        print('Nothing has changed')
-        ios.discard_config()
+
+
+    #NAPALM Driver Cred to Access Cisco IOS Device
+    devicelist = ['10.3.255.102']
+
+
+    #Loop trough each IP Address and validate the current config against the template
+    for ip_address in devicelist:
+        print (str(datetime.now()) + " Connecting to " + str(ip_address))
+        print("==============================================================")
+        driver = napalm.get_network_driver('ios')
+        ios = driver(ip_address, 'python', 'cisco')
+        ios.open()
+        #Replacing the config iff the difference is > than 0 characters
+        ios.load_replace_candidate(config= rendered_config)
+        diffs = ios.compare_config()
+        if len(diffs) > 0:
+            print('config has changed')
+            print("==============================================================")
+            print("===========================Changes:===========================")
+            print(diffs)
+            print("==============================================================")
+            ios.commit_config()
+        else:
+            print('Nothing has changed')
+            ios.discard_config()
+    sleep(60)
